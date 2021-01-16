5d44b35c89817e9c9d29e7eb1f3a47faf17d006a
VishwajeetLatpate <vlatpate@gmail.com>
2021-01-16 08:34:46 +0530

    Pandit selection

--- src/app/loginsuccess/loginsuccess.component.ts
+++ src/app/loginsuccess/loginsuccess.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+/* import { Component, OnInit } from '@angular/core';
 import { Pandit } from '../pandit';
 import { RegistrationService } from '../registration.service';
 
@@ -29,3 +29,48 @@ pandit =new Pandit();
  
   
 }
+*/
+import { Component, OnInit } from '@angular/core';
+import { Pandit } from '../pandit';
+import { RegistrationService } from '../registration.service';
+
+@Component({
+  selector: 'app-loginsuccess',
+  templateUrl: './loginsuccess.component.html',
+  styleUrls: ['./loginsuccess.component.css']
+})
+export class LoginsuccessComponent implements OnInit {
+  private pandit: Pandit = new Pandit();
+  public vidhiName: string = "";
+  public data = [];
+  public noData: any;
+  public results = [];
+  public vidhiOptions = [
+    { id: 0, name: "Satyanarayan" },
+    { id: 1, name: "Vastu Puja" },
+    { id: 2, name: "Durga Puja" },
+    { id: 3, name: "Vivah Puja" },
+    { id: 4, name: "Laxmi Puja" },
+    { id: 5, name: "Ghat Puja" },
+    { id: 6, name: "Shradh" },
+  ];
+
+  map: any;
+  constructor(private api: RegistrationService) { }
+
+  getAll() {
+    // VidhiName will be updated in model when selection is changed 
+    // and available as this.vidhiName 
+    this.api.getPanditFromRemote(this.vidhiName).subscribe((results) => {
+      this.data = results;
+      console.log(this.data);
+      console.log('JSON Response = ', JSON.stringify(results));
+    })
+  }
+
+  ngOnInit() {
+  }
+
+
+}
+
--- src/app/registration.service.ts
+++ src/app/registration.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+/* import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { Client } from './client';
 import { catchError, retry } from 'rxjs/operators';
@@ -9,9 +9,9 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 })
 export class RegistrationService {
   
-/* 
+
 getAll(): Observable<any> {
-    return this.http.get(this.api)*/  
+    return this.http.get(this.api) 
   constructor(private _http : HttpClient) { }
   public loginClientRemote(client :Client):Observable<any>{
       return this._http.post("http://localhost:8080/login",client);
@@ -40,3 +40,49 @@ getAll(): Observable<any> {
 
 
 }
+
+*/
+
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { Client } from './client';
+import { catchError, retry } from 'rxjs/operators';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegistrationService {
+  
+/* 
+getAll(): Observable<any> {
+    return this.http.get(this.api)*/  
+  constructor(private _http : HttpClient) { }
+  public loginClientRemote(client :Client):Observable<any>{
+      return this._http.post("http://localhost:8080/login",client);
+  }
+
+  public registerClientFromRemote(client :Client):Observable<any>{
+    return this._http.post("http://localhost:8080/registerclient",client);
+}
+
+ public getPanditFromRemote(vidhiName :String) :Observable<any>{
+  return this._http.get(`http://localhost:8080/pandit?vidhiName=${vidhiName}`)
+  .pipe(
+    catchError(this.handleError)
+  );
+ }
+ private handleError(error: HttpErrorResponse) {
+  if (error.error instanceof ErrorEvent) {
+    console.log(error.error.message)
+
+  } else {
+    console.log(error.status)
+  }
+  return throwError(
+    console.log('Something is wrong!'));
+};
+
+
+}
+
