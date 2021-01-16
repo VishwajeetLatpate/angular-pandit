fdc252c3fb087bab4f8e77ed98094cc0d92e83c7
VishwajeetLatpate <vlatpate@gmail.com>
2021-01-16 08:38:58 +0530

    changes

--- src/app/loginsuccess/loginsuccess.component.ts
+++ src/app/loginsuccess/loginsuccess.component.ts
@@ -1,35 +1,3 @@
-/* import { Component, OnInit } from '@angular/core';
-import { Pandit } from '../pandit';
-import { RegistrationService } from '../registration.service';
-
-@Component({
-  selector: 'app-loginsuccess',
-  templateUrl: './loginsuccess.component.html',
-  styleUrls: ['./loginsuccess.component.css']
-})
-export class LoginsuccessComponent implements OnInit {
-pandit =new Pandit();
- msg='';
- public data = [];
- public noData: any;
- public results = [];
-  map: any;
-  constructor( private api: RegistrationService ) { }
-
-  getAll() {
-    this.api.getPanditFromRemote().subscribe((results) =>  {
-      this.data = results;
-      console.log(this.data);
-      console.log('JSON Response = ', JSON.stringify(results));
-    })
-  }
-
-  ngOnInit() {
-  }
- 
-  
-}
-*/
 import { Component, OnInit } from '@angular/core';
 import { Pandit } from '../pandit';
 import { RegistrationService } from '../registration.service';
